b5b89b328b13b0b7457aa3f80b89fb2eb2d790ce
Ashish Jayaram <49029597+AshishJayaram@users.noreply.github.com>
2019-09-13 09:55:10 +0530

    Delete 9.py

--- 9.py
+++
@@ -1,9 +0,0 @@
-def rem_dup(numbers):
-    list = []
-    for i in numbers:
-        if i not in list:
-            list.append(i)
-
-    print (list)
-    return list
-rem_dup([1,2,3,4,5,6,3,7,2,3,7])
