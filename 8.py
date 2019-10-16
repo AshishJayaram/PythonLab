911e8d71a1bf9e0cebdf34877c46ee7a41f55a93
Ashish Jayaram <49029597+AshishJayaram@users.noreply.github.com>
2019-10-16 10:07:32 +0530

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,15 +1,16 @@
+
 class Student:
-    def __init__(self,name,age):
+    def __init__(self,name,age,marks):
         self.name=name;
         self.age=age;
-    marks=["91","92","93"];
+        self.marks=marks;
 
-p1=Student("Sponge",20,)
+p1=Student("Sponge",20,[91,92,93])
 #p2=Student("DFJ",20)
 
 def Display(Student):
     print("\nName of person is ", p1.name)
     print("\nAge of person is ", p1.age)
-    print(Student.marks)
+    print("\nMarks are ",p1.marks)
 
 Display(Student)
